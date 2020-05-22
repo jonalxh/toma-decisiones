127b8fa205635106c54587ded7651b3daf1bbd1e
Jonathan Arias <jonalxh@gmail.com>
2020-05-22 12:18:17 -0500

    gh-pages

---
+++ precache-manifest.e0516d485c9811f26b1ccdffb74701a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec5d0abb7d8c0fd4be4",
+    "url": "/toma-decisiones/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "fe9b0813eb9bbc704286",
+    "url": "/toma-decisiones/css/chunk-vendors.0a1ff800.css"
+  },
+  {
+    "revision": "12e1670eb119d16255338d261e0fcee9",
+    "url": "/toma-decisiones/index.html"
+  },
+  {
+    "revision": "43b4691722bfdebd3ed7",
+    "url": "/toma-decisiones/js/about.5d9c2b81.js"
+  },
+  {
+    "revision": "3ec5d0abb7d8c0fd4be4",
+    "url": "/toma-decisiones/js/app.3076fb8f.js"
+  },
+  {
+    "revision": "fe9b0813eb9bbc704286",
+    "url": "/toma-decisiones/js/chunk-vendors.210fd5a7.js"
+  },
+  {
+    "revision": "94e1259de85a3d2f4dd7501bdb7c202d",
+    "url": "/toma-decisiones/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/toma-decisiones/robots.txt"
+  }
+]);
